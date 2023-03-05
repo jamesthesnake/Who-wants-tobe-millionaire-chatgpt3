414932a9ba89ad19c1de7611a29d0982cf4a3a4c
James Hennessy <james.ryan.hennessy@gmail.com>
2023-03-05 15:17:09 -0800

    Add files via upload

---
+++ roo.py
@@ -0,0 +1,117 @@
+import requests
+import openai
+import json
+from PIL import Image
+import io
+import stability_sdk
+API_KEY="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"
+import os
+import warnings
+from PIL import Image
+from stability_sdk import client
+import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
+
+# Our Host URL should not be prepended with "https" nor should it have a trailing slash.
+os.environ['STABILITY_HOST'] = 'grpc.stability.ai:443'
+
+# Sign up for an account at the following link to get an API Key.
+# https://beta.dreamstudio.ai/membership
+
+# Click on the following link once you have created an account to be taken to your API Key.
+# https://beta.dreamstudio.ai/membership?tab=apiKeys
+os.environ['STABILITY_KEY'] = 'sk-PKxgympH4vzv6y2Dhqf8lgghFVMdiCKFdhXqw94TwE66JdQW'
+
+# Paste your API Key below.
+# Set up our connection to the API.
+stability_api = client.StabilityInference(
+    key=os.environ['STABILITY_KEY'], # API Key reference.
+    verbose=True, # Print debug messages.
+    engine="stable-diffusion-v1-5", # Set the engine to use for generation.
+    # Available engines: stable-diffusion-v1 stable-diffusion-v1-5 stable-diffusion-512-v2-0 stable-diffusion-768-v2-0
+    # stable-diffusion-512-v2-1 stable-diffusion-768-v2-1 stable-inpainting-v1-0 stable-inpainting-512-v2-0
+)
+# Set up our initial generation parameters.
+answers = stability_api.generate(
+    prompt=" regis philbin hosting who wants to be a millionaire, artstation, masterful, ghibli",
+    seed=992441358, # If a seed is provided, the resulting generated image will be deterministic.
+                    # What this means is that as long as all generation parameters remain the same, you can always recall the same image simply by generating it again.
+                    # Note: This isn't quite the case for Clip Guided generations, which we'll tackle in a future example notebook.
+    steps=30, # Amount of inference steps performed on image generation. Defaults to 30.
+    cfg_scale=8.0, # Influences how strongly your generation is guided to match your prompt.
+                   # Setting this value higher increases the strength in which it tries to match your prompt.
+                   # Defaults to 7.0 if not specified.
+    width=512, # Generation width, defaults to 512 if not included.
+    height=512, # Generation height, defaults to 512 if not included.
+    samples=1, # Number of images to generate, defaults to 1 if not included.
+    sampler=generation.SAMPLER_K_DPMPP_2M # Choose which sampler we want to denoise our generation with.
+                                                 # Defaults to k_dpmpp_2m if not specified. Clip Guidance only supports ancestral samplers.
+                                                 # (Available Samplers: ddim, plms, k_euler, k_euler_ancestral, k_heun, k_dpm_2, k_dpm_2_ancestral, k_dpmpp_2s_ancestral, k_lms, k_dpmpp_2m)
+)
+
+# Set up our warning to print to the console if the adult content classifier is tripped.
+# If adult content classifier is not tripped, save generated images.
+for resp in answers:
+    for artifact in resp.artifacts:
+        if artifact.finish_reason == generation.FILTER:
+            warnings.warn(
+                "Your request activated the API's safety filters and could not be processed."
+                "Please modify the prompt and try again.")
+        if artifact.type == generation.ARTIFACT_IMAGE:
+            img = Image.open(io.BytesIO(artifact.binary))
+            img.save(str(artifact.seed)+ ".png") # Save our generated images with their seed number as the filename.
+
+# Define OpenAI API key 
+openai.api_key = API_KEY
+
+# Set up the model and prompt
+model_engine = "text-davinci-003"
+prompt = "please pretend to be regis philibin and host who wants to be a millionaire and then ask me a question , do not give me the correct answer till I respond, and give me four solutions , three false and one correct!Be sure tos ay your name! Also keep score!"
+
+# Generate a response
+
+messages = [
+    {"role": "system", "content": "You are a kind helpful assistant."},
+]
+while True:
+    message = input("User : ")
+    if message:
+        messages.append(
+            {"role": "user", "content": message},
+        )
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages
+        )
+    
+    reply = chat.choices[0].message.content
+    print(f"ChatGPT: {reply}")
+    messages.append({"role": "assistant", "content": reply})
+     
+ 
+
+""""
+completion = openai.ChatCompletion.create(
+    engine=model_engine,
+    prompt=prompt,
+    max_tokens=1024,
+    n=1,
+    stop=None,
+    temperature=0.5,
+)
+response = completion.choices[0].text
+print(dir(completion))
+print(response)
+while True:
+    message = input("User : ")
+    if message:
+        completion.append(
+            {"role": "user", "content": message},
+        )
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=completion
+        )
+    
+    response = completion.choices[0].text
+    print(f"ChatGPT: {response}")
+    completion.append({"role": "assistant", "content": response})
+
+"""
